b33051481624cb45e45046df2cad4a17406925f3
Norde <6858706+Norde@users.noreply.github.com>
2024-08-24 22:01:28 +0000

    Update restart-interface.sh
    Change wwan interfance to wan

--- restart-interface.sh
+++ restart-interface.sh
@@ -2,10 +2,10 @@
 # This file is responsible for restarting the network interface.
 # Should be run once OFFLINE state is detected.
 
-INTERFACE="wwan"
+INTERFACE="wan"
 
 # syslog entry
-logger -s "INTERNET KEEP ALIVE SYSTEM: Restarting the LTE interface."
+logger -s "INTERNET KEEP ALIVE SYSTEM: Restarting the Wan interface."
 
 echo "SH RESTART IFACE DOWN"
 ifdown $INTERFACE
