56fe845115152e0f293b84c0592111931cfc6199
Norde <6858706+Norde@users.noreply.github.com>
2024-08-25 13:19:48 +0000

    Update dns-test.sh
    - Use up to date OpenWRT ncat package
    - Add a second IP to scan in case of failure of the first one (Quad9 and DNS.EU dns)
    - Increase packet count to 5

--- dns-test.sh
+++ dns-test.sh
@@ -1,25 +1,41 @@
-#!/bin/ash
-# This file is responsible for DNS check. The return value of its process
-# determines the ONLINE/OFFLINE state.
+#!/bin/sh
+# This file is responsible for DNS check. The return value of its process determines the ONLINE (exit 0)/OFFLINE (exit 1) state.
+# OpenWRT ncat package is used instead of the included busybox "compact" version or outdated netcat package. 
 
-IP_TO_PING=9.9.9.9
+IP1_TO_SCAN=9.9.9.9 #Quad9 DNS
+IP2_TO_SCAN=193.110.81.0 #DNS0.EU
 PACKET_COUNT=5
 
-ONLINE=0
+ONLINE1=0
+ONLINE2=0
 
-for i in `seq 1 $PACKET_COUNT`;
-        do
-                nc -G 2 -z $IP_TO_PING 53
-                RETVAL=$?
-				if [ $RETVAL -eq 0 ]; then
-					ONLINE=1
-				fi
-        done
+for i in "seq 1 $PACKET_COUNT";
+        do
+                ncat -G 4 -z $IP1_TO_SCAN 53
+                RETVAL1=$?
+                if [ $RETVAL1 -eq 0 ]; then
+                    ONLINE1=1
+                fi
+        done
 
-if [ $ONLINE -eq 1 ]; then
-    # ONLINE
-    exit 0
+if [ $ONLINE1 -eq 1 ]; then
+    # IP1_TO_SCAN is reacheable
+    exit 0
+fi
+
+
+for i in "seq 1 $PACKET_COUNT";
+        do
+                ncat -G 4 -z $IP2_TO_SCAN 53
+                RETVAL2=$?
+                if [ $RETVAL2 -eq 0 ]; then
+                    ONLINE2=1
+                fi
+        done
+if [ $ONLINE2 -eq 1 ]; then
+    # IP2_TO_SCAN is reacheable
+    exit 0
 else
-    # OFFLINE
-    exit 1
+    # OFFLINE (connexion is down)
+    exit 1
 fi
