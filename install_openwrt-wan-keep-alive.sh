1b852a0adf5b31b9d7a9332363b7a45b17aa4581
Norde <6858706+Norde@users.noreply.github.com>
2024-09-29 19:09:26 +0000

    Update install_openwrt-wan-keep-alive.sh
    Add --no-cache option to all wget to prevent cache issue.
    Update typo
    Add abort notification for yn part

--- install_openwrt-wan-keep-alive.sh
+++ install_openwrt-wan-keep-alive.sh
@@ -29,11 +29,11 @@ download_files()
     mkdir $DIR
     touch $DIR/log.txt
     echo "Downloading files from https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master ..."
-    wget -q --no-check-certificate https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master/dns-test.sh -O $DIR/dns-test.sh && chmod +x $DIR/dns-test.sh
-    wget -q --no-check-certificate https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master/wan-keep-alive.sh -O $DIR/wwan-keep-alive.sh && chmod +x $DIR/wwan-keep-alive.sh
-    wget -q --no-check-certificate https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master/restart-interface.sh -O $DIR/restart-interface.sh && chmod +x $DIR/restart-interface.sh
-    wget -q --no-check-certificate https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master/restart-router.sh -O $DIR/restart-router.sh && chmod +x $DIR/restart-router.sh
-    wget -q --no-check-certificate https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master/wankeepalive -O /etc/init.d/wankeepalive && chmod +x /etc/init.d/wankeepalive
+    wget -q --no-check-certificate --no-cache https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master/dns-test.sh -O $DIR/dns-test.sh && chmod +x $DIR/dns-test.sh
+    wget -q --no-check-certificate --no-cache https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master/wan-keep-alive.sh -O $DIR/wwan-keep-alive.sh && chmod +x $DIR/wwan-keep-alive.sh
+    wget -q --no-check-certificate --no-cache https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master/restart-interface.sh -O $DIR/restart-interface.sh && chmod +x $DIR/restart-interface.sh
+    wget -q --no-check-certificate --no-cache https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master/restart-router.sh -O $DIR/restart-router.sh && chmod +x $DIR/restart-router.sh
+    wget -q --no-check-certificate --no-cache https://raw.githubusercontent.com/Norde/openwrt-wan-keep-alive/master/wankeepalive -O /etc/init.d/wankeepalive && chmod +x /etc/init.d/wankeepalive
     finish
 }
 
@@ -41,10 +41,10 @@ download_files()
 echo "Checking for ncat package: $NCAT_INSTALLED"
 if [ "" = "$NCAT_INSTALLED" ]; then
     echo "ncat package is not installed"
-    read -p "This will install ncat package as a prerequisite. Do you want to continue (y/n)?" yn
+    read -p "This will install ncat package as a prerequisite. Do you want to continue (y/n)? " yn
     case $yn in
         [Yy]* ) install_ncat; break;;
-        [Nn]* ) exit;;
+        [Nn]* ) echo "Installation aborted by user" ; exit;;
         * ) echo "Please answer 'y' or 'n'.";;
     esac
 fi
@@ -55,7 +55,7 @@ while true; do
     read -p "This will download the files into $DIR. Do you want to continue (y/n)? " yn
     case $yn in
         [Yy]* ) download_files; break;;
-        [Nn]* ) exit;;
+        [Nn]* ) echo "Installation aborted by user" ; exit;;
         * ) echo "Please answer 'y' or 'n'.";;
     esac
 done
