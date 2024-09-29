ee08987021dfa2d4b001683e4a4c8e60c17a1da5
Norde <6858706+Norde@users.noreply.github.com>
2024-09-29 18:38:14 +0000

    Update wan-keep-alive.sh
    Typo correction

--- wan-keep-alive.sh
+++ wan-keep-alive.sh
@@ -15,30 +15,31 @@ LINES_MAX=11000
 LINES_MIN=6000
 LINES_COUNT=$(wc -l $LOG_FILE | awk '{print $1}')
 
-until [  ];do
-	# if the log files gets huge, strip it, keep last LINES_MIN lines
-	if [[ "$LINES_COUNT" -ge "$LINES_MAX" ]]; then
-		echo "$(tail -$LINES_MIN $LOG_FILE)" > $LOG_FILE
-	fi
+until [  ];do
+    # if the log files gets huge, strip it, keep last LINES_MIN lines
+    if [[ "$LINES_COUNT" -ge "$LINES_MAX" ]]; then
+        echo "$(tail -$LINES_MIN $LOG_FILE)" > $LOG_FILE
+    fi
 
-	# DNS test, it's result defines the ONLINE/OFFLINE state
-	`$SH_DNS_TESTS`
+    # DNS test, it's result defines the ONLINE/OFFLINE state
+    `$SH_DNS_TESTS`
 
-	if [ $? -eq 1 ]; then
-		echo "Ooops, we're offline!"
-		echo "$(date) OFFLINE > RESTARTING INTERFACE" >> $LOG_FILE
+    if [ $? -eq 1 ]; then
+        echo "Router is offline !."
+        echo "$(date) OFFLINE > Restarting interface" >> $LOG_FILE
 
-		if [[ "$OFFLINE_COUNT" -ge "$OFFLINE_COUNT_TRESHOLD" ]]; then
-			echo ">> Restarting router.."
-			sleep 5m
-			$SH_RESTART_ROUTER
-		else
-			echo ">> Restarting interface.."
-			$SH_RESTART_INTERFACE
-		fi
-	else
-		echo "We're okay!"
-		echo "$(date) ONLINE" >> $LOG_FILE
-		sleep 2m
-	fi
+        if [[ "$OFFLINE_COUNT" -ge "$OFFLINE_COUNT_TRESHOLD" ]]; then
+            echo ">> Restarting router..."
+            sleep 5m
+            $SH_RESTART_ROUTER
+        else
+            echo ">> Restarting interface..."
+            $SH_RESTART_INTERFACE
+            sleep 45s
+        fi
+    else
+        echo "Router is online :)"
+        echo "$(date) ONLINE" >> $LOG_FILE
+        sleep 2m
+    fi
 done
