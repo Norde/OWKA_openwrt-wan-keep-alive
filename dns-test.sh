5a8a24220cca8858548fc05bf9f5876a22ab5ffd
Norde <6858706+Norde@users.noreply.github.com>
2024-09-30 20:57:24 +0000

    Correction of wrong tab caracter (char encoding bug)

--- dns-test.sh
+++ dns-test.sh
@@ -10,32 +10,32 @@ ONLINE1=0
 ONLINE2=0
 
 for i in "seq 1 $PACKET_COUNT";
-        do
-                ncat -G 4 -z $IP1_TO_SCAN 53
-                RETVAL1=$?
-                if [ $RETVAL1 -eq 0 ]; then
-                    ONLINE1=1
-                fi
-        done
+    do
+        ncat -G 4 -z $IP1_TO_SCAN 53
+        RETVAL1=$?
+        if [ $RETVAL1 -eq 0 ]; then
+            ONLINE1=1
+        fi
+    done
 
 if [ $ONLINE1 -eq 1 ]; then
-    # IP1_TO_SCAN is reacheable
-    exit 0
+    # IP1_TO_SCAN is reacheable
+    exit 0
 fi
 
 
 for i in "seq 1 $PACKET_COUNT";
-        do
-                ncat -G 4 -z $IP2_TO_SCAN 53
-                RETVAL2=$?
-                if [ $RETVAL2 -eq 0 ]; then
-                    ONLINE2=1
-                fi
-        done
+    do
+        ncat -G 4 -z $IP2_TO_SCAN 53
+        RETVAL2=$?
+        if [ $RETVAL2 -eq 0 ]; then
+            ONLINE2=1
+        fi
+    done
 if [ $ONLINE2 -eq 1 ]; then
-    # IP2_TO_SCAN is reacheable
-    exit 0
+    # IP2_TO_SCAN is reacheable
+    exit 0
 else
-    # OFFLINE (connexion is down)
-    exit 1
+    # OFFLINE (connexion is down)
+    exit 1
 fi
